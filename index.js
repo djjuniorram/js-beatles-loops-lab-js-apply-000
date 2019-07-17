7e03c7441bfac6449cc32a4100e1dc209abc908a
Ramesh Singh <rameshsingh.jr@gmail.com>
2019-07-17 19:32:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < 4; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var array = [+ facts + "!!!"];
+  let i = 0;
+  while (i < array.length) {
+    console.log(facts + "!!!");
+  }
+  return array;
+}
+
